45e4d57366f0fab7b204450de7dcff8f116c3947
sanjit nc <njsa3803@gmail.com>
2026-05-08 22:34:21 +0530

    removed api import

--- core/urls.py
+++ core/urls.py
@@ -1,32 +1,13 @@
 from django.urls import path
+from django.http import HttpResponse
 
-from .views import send_email
-
-from django.contrib import admin
-from django.urls import path
-
+def home(request):
+    return HttpResponse("Frontend Working 🚀")
 
 urlpatterns = [
     path('', home),
-    path('admin/', admin.site.urls),
-    path('api/send/', send_email),
 ]
-from .views import (
-    home,
-    send_email_api,
-    get_all_emails,
-    get_single_email,
-    delete_email
-)
 
-urlpatterns = [
-    path("", home),
 
-    path("send-email/", send_email_api),
 
-    path("emails/", get_all_emails),
 
-    path("emails/<int:id>/", get_single_email),
-
-    path("emails/delete/<int:id>/", delete_email),
-]
