60171b3a72a9efcadb4b80e1832c5fcc365b37b4
sanjit nc <njsa3803@gmail.com>
2026-05-08 21:31:37 +0530

    added home route

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Django Backend Running Successfully!")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("core.urls")),
+    path('', home),
+    path('admin/', admin.site.urls),
+    path('api/', include('yourapp.urls')),
 ]
