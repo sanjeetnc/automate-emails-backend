d5e8192706785cbfab601f26126e34f9ea268912
sanjit nc <njsa3803@gmail.com>
2026-05-08 22:54:26 +0530

    homepage restored

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+from core.views import send_email
+
+def home(request):
+    return HttpResponse("""
+    <h1>Frontend Working 🚀</h1>
+
+    <button onclick="sendMail()">
+        Send Email
+    </button>
+
+    <script>
+
+    async function sendMail() {
+
+        const response = await fetch('/send/');
+
+        const data = await response.json();
+
+        alert(data.message);
+    }
+
+    </script>
+    """)
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', home),
+    path('send/', send_email),
     path('admin/', admin.site.urls),
 ]
